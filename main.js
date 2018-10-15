5129ab8fef285b4da7cd2caa5e04ce351d91e2d9
JunKulung <41711587+JunKulung@users.noreply.github.com>
2018-10-15 19:02:38 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+
+        $(document).ready(function() {
+            $(window).scroll(function () {
+                if ($(this).scrollTop() > 100) {
+                $(".navbar").css("max-height","64%");
+         
+         
+            } else {
+                $(".navbar").css("max-height","0");
+                }
+         })
+         })
+    
+      
+
+
